12e9f7defc1df72c15389b98da9ed7a3d4b524de
Rob Winch <rwinch@users.noreply.github.com>
2019-11-05 10:42:25 -0600

    Only require registration once

--- src/main/java/example/webauthn/Mfa.java
+++ src/main/java/example/webauthn/Mfa.java
@@ -1,7 +1,10 @@
 package example.webauthn;
 
+import com.webauthn4j.authenticator.Authenticator;
 import example.webauthn.security.MultiFactorAuthentication;
+import example.webauthn.security.MultiFactorAuthenticationRequiredException;
 import example.webauthn.security.MultiFactorRegistrationRequiredException;
+import example.webauthn.security.WebAuthnAuthenticatorRepository;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -11,13 +14,23 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class Mfa {
+	private final WebAuthnAuthenticatorRepository authenticators;
+
+	public Mfa(WebAuthnAuthenticatorRepository authenticators) {
+		this.authenticators = authenticators;
+	}
+
 	public boolean require(Authentication authentication) {
 		if (authentication instanceof AnonymousAuthenticationToken) {
 			return false;
 		}
-		if (!(authentication instanceof MultiFactorAuthentication)) {
+		if (authentication instanceof MultiFactorAuthentication) {
+			return true;
+		}
+		Authenticator authenticator = this.authenticators.load(authentication);
+		if (authenticator == null) {
 			throw new MultiFactorRegistrationRequiredException();
 		}
-		return true;
+		throw new MultiFactorAuthenticationRequiredException();
 	}
 }
