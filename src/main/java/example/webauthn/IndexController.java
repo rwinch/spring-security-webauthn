386e5bba3b23396a4ccd5b383c26b163087a7011
Rob Winch <rwinch@users.noreply.github.com>
2019-11-05 10:51:06 -0600

    Clean Imports

--- src/main/java/example/webauthn/IndexController.java
+++ src/main/java/example/webauthn/IndexController.java
@@ -1,11 +1,7 @@
 package example.webauthn;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpSession;
-import java.util.Map;
 
 /**
  * @author Rob Winch
