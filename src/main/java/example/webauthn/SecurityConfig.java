c4ee7a14a315a99c7bc7c0ac1e0055cc4855d4c3
Rob Winch <rwinch@users.noreply.github.com>
2019-11-08 15:46:53 -0600

    Add WebAuthnConfigurer

--- src/main/java/example/webauthn/SecurityConfig.java
+++ src/main/java/example/webauthn/SecurityConfig.java
@@ -1,25 +1,10 @@
 package example.webauthn;
 
-import example.webauthn.security.WebAuthnAuthenticatorRepository;
-import example.webauthn.security.web.DefaultWebAuthnLoginPageGeneratingFilter;
-import example.webauthn.security.web.DefaultWebAuthnRegistrationGeneratingFilter;
-import example.webauthn.security.web.MultiFactorExceptionTranslationFilter;
-import example.webauthn.security.web.WebAuthnChallengeRepository;
-import example.webauthn.security.web.WebAuthnLoginFilter;
-import example.webauthn.security.web.WebAuthnRegistrationFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.access.ExceptionTranslationFilter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.ui.DefaultLoginPageGeneratingFilter;
-import org.springframework.security.web.csrf.CsrfToken;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Collections;
-import java.util.Map;
-import java.util.function.Function;
+import static example.webauthn.security.WebAuthnConfigurer.webAuthn;
 
 /**
  * @author Rob Winch
@@ -27,32 +12,11 @@ import java.util.function.Function;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private WebAuthnChallengeRepository challenges;
-
-	@Autowired
-	private WebAuthnAuthenticatorRepository authenticators;
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		Function<HttpServletRequest, Map<String, String>> csrf = request -> {
-			CsrfToken token = (CsrfToken)request.getAttribute(CsrfToken.class.getName());
-			return token == null ? Collections.emptyMap() : Collections.singletonMap(token.getParameterName(), token.getToken());
-		};
-		DefaultWebAuthnRegistrationGeneratingFilter registration = new DefaultWebAuthnRegistrationGeneratingFilter();
-		registration.setResolveHiddenInputs(csrf);
-		DefaultWebAuthnLoginPageGeneratingFilter login = new DefaultWebAuthnLoginPageGeneratingFilter(
-				authenticators);
-		login.setResolveHiddenInputs(csrf);
 		http
-			.addFilterBefore(registration,
-					DefaultLoginPageGeneratingFilter.class)
-			.addFilterBefore(login,
-					DefaultLoginPageGeneratingFilter.class)
-			.addFilterAfter(new MultiFactorExceptionTranslationFilter(), ExceptionTranslationFilter.class)
-			.addFilterBefore(new WebAuthnRegistrationFilter(this.challenges, this.authenticators), UsernamePasswordAuthenticationFilter.class)
-			.addFilterBefore(new WebAuthnLoginFilter(this.challenges, this.authenticators),
-					UsernamePasswordAuthenticationFilter.class)
+			.apply(webAuthn())
+				.and()
 			.formLogin().and()
 			.authorizeRequests()
 				.mvcMatchers("/secure").access("@mfa.require(authentication)")
---
+++ src/main/java/example/webauthn/security/WebAuthnConfigurer.java
@@ -0,0 +1,63 @@
+package example.webauthn.security;
+
+import example.webauthn.security.web.DefaultWebAuthnLoginPageGeneratingFilter;
+import example.webauthn.security.web.DefaultWebAuthnRegistrationGeneratingFilter;
+import example.webauthn.security.web.MultiFactorExceptionTranslationFilter;
+import example.webauthn.security.web.WebAuthnChallengeRepository;
+import example.webauthn.security.web.WebAuthnLoginFilter;
+import example.webauthn.security.web.WebAuthnRegistrationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.ui.DefaultLoginPageGeneratingFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * @author Rob Winch
+ */
+public class WebAuthnConfigurer extends
+		AbstractHttpConfigurer<WebAuthnConfigurer, HttpSecurity> {
+
+	public static WebAuthnConfigurer webAuthn() {
+		return new WebAuthnConfigurer();
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		WebAuthnAuthenticatorRepository authenticators = authnAuthenticatorRepository();
+		WebAuthnChallengeRepository challenges = challengeRepository();
+		Function<HttpServletRequest, Map<String, String>> csrf = request -> {
+			CsrfToken token = (CsrfToken)request.getAttribute(CsrfToken.class.getName());
+			return token == null ? Collections.emptyMap() : Collections.singletonMap(token.getParameterName(), token.getToken());
+		};
+		DefaultWebAuthnRegistrationGeneratingFilter registration = new DefaultWebAuthnRegistrationGeneratingFilter();
+		registration.setResolveHiddenInputs(csrf);
+		DefaultWebAuthnLoginPageGeneratingFilter login = new DefaultWebAuthnLoginPageGeneratingFilter(
+				authenticators);
+		login.setResolveHiddenInputs(csrf);
+		http
+				.addFilterBefore(registration,
+						DefaultLoginPageGeneratingFilter.class)
+				.addFilterBefore(login,
+						DefaultLoginPageGeneratingFilter.class)
+				.addFilterAfter(new MultiFactorExceptionTranslationFilter(), ExceptionTranslationFilter.class)
+				.addFilterBefore(new WebAuthnRegistrationFilter(challenges, authenticators), UsernamePasswordAuthenticationFilter.class)
+				.addFilterBefore(new WebAuthnLoginFilter(challenges, authenticators),
+						UsernamePasswordAuthenticationFilter.class);
+	}
+
+	private WebAuthnChallengeRepository challengeRepository() {
+		return new WebAuthnChallengeRepository();
+	}
+
+	WebAuthnAuthenticatorRepository authnAuthenticatorRepository() {
+		return new WebAuthnAuthenticatorRepository();
+	}
+}
