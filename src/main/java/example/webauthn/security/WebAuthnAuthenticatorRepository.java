4a873d7b84633db4698dd083475aea3b3e383415
ynojima <mail@ynojima.net>
2020-05-14 14:27:51 +0900

    Optimise imports

--- src/main/java/example/webauthn/security/WebAuthnAuthenticatorRepository.java
+++ src/main/java/example/webauthn/security/WebAuthnAuthenticatorRepository.java
@@ -1,21 +1,7 @@
 package example.webauthn.security;
 
-import com.webauthn4j.authenticator.Authenticator;
-import com.webauthn4j.authenticator.AuthenticatorImpl;
-import com.webauthn4j.converter.AttestationObjectConverter;
-import com.webauthn4j.converter.CollectedClientDataConverter;
-import com.webauthn4j.converter.util.CborConverter;
-import com.webauthn4j.converter.util.JsonConverter;
-import com.webauthn4j.data.WebAuthnRegistrationContext;
-import com.webauthn4j.data.attestation.AttestationObject;
-import com.webauthn4j.data.attestation.authenticator.AuthenticatorData;
-import com.webauthn4j.data.client.CollectedClientData;
-import com.webauthn4j.data.extension.authenticator.RegistrationExtensionAuthenticatorOutput;
-import com.webauthn4j.validator.WebAuthnRegistrationContextValidationResponse;
-import com.webauthn4j.validator.WebAuthnRegistrationContextValidator;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.webauthn.AuthenticatorAttestationResponse;
-import org.springframework.stereotype.Component;
 
 /**
  * FIXME: This should be saved per Authentication
