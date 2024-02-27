58a582350438faf636c249c033dee061307cc69a
Rob Winch <rwinch@users.noreply.github.com>
2024-02-27 10:34:21 -0600

    Add TestUserCredential

---
+++ src/test/java/org/springframework/security/webauthn/api/TestUserCredential.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2002-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.security.webauthn.api;
+
+import org.springframework.security.webauthn.api.core.ArrayBuffer;
+import org.springframework.security.webauthn.api.core.BufferSource;
+import org.springframework.security.webauthn.management.ImmutablePublicKeyCose;
+import org.springframework.security.webauthn.management.ImmutableUserCredential;
+import org.springframework.security.webauthn.management.OptionalBoolean;
+
+public class TestUserCredential {
+	public static ImmutableUserCredential.ImmutableUserCredentialBuilder userCredential() {
+		return ImmutableUserCredential.builder()
+				.label("label")
+				.credentialId(ArrayBuffer.fromBase64("NauGCN7bZ5jEBwThcde51g"))
+				.userEntityUserId(BufferSource.fromBase64("vKBFhsWT3gQnn-gHdT4VXIvjDkVXVYg5w8CLGHPunMM"))
+				.publicKeyCose(ImmutablePublicKeyCose.fromBase64("pQECAyYgASFYIC7DAiV_trHFPjieOxXbec7q2taBcgLnIi19zrUwVhCdIlggvN6riHORK_velHcTLFK_uJhyKK0oBkJqzNqR2E-2xf8="))
+				.backupEligible(OptionalBoolean.fromBoolean(true))
+				.backupState(OptionalBoolean.fromBoolean(true));
+	}
+}
