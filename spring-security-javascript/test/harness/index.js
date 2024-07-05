363e9ca97260ade309d8f20c47ffb8ee5ed079bf
Daniel Garnier-Moiroux <git@garnier.wf>
2024-07-05 11:51:39 +0200

    simplify package.json

--- spring-security-javascript/test/harness/index.js
+++ spring-security-javascript/test/harness/index.js
@@ -5,11 +5,4 @@ process.env.NODE_ENV = 'test'
 
 const chai = require('chai')
 
-chai.use(require('chai-fs'))
-chai.use(require('chai-spies'))
-// dirty-chai must be loaded after the other plugins
-// see https://github.com/prodatakey/dirty-chai#plugin-assertions
-chai.use(require('dirty-chai'))
-
-
 module.exports = { expect: chai.expect, spy: chai.spy }
