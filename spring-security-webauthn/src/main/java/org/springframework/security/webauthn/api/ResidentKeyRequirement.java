ee33a67d8171e59471765cc579d12418982518a1
Rob Winch <362503+rwinch@users.noreply.github.com>
2024-09-19 13:33:44 -0500

    ResidentKeyRequirement enum->class

--- spring-security-webauthn/src/main/java/org/springframework/security/webauthn/api/ResidentKeyRequirement.java
+++ spring-security-webauthn/src/main/java/org/springframework/security/webauthn/api/ResidentKeyRequirement.java
@@ -22,31 +22,31 @@ package org.springframework.security.webauthn.api;
  * @since 6.4
  * @author Rob Winch
  */
-public enum ResidentKeyRequirement {
+public final class ResidentKeyRequirement {
 
 	/**
 	 * The <a href="https://www.w3.org/TR/webauthn-3/#dom-residentkeyrequirement-discouraged">discouraged</a> requirement
 	 * indicates that the Relying Party prefers creating a server-side credential, but will accept a client-side
 	 * discoverable credential.
 	 */
-	DISCOURAGED("discouraged"),
+	public static final ResidentKeyRequirement DISCOURAGED = new ResidentKeyRequirement("discouraged");
 
 	/**
 	 * The <a href="https://www.w3.org/TR/webauthn-3/#dom-residentkeyrequirement-preferred">preferred</a> requirement
 	 * indicates that the Relying Party strongly prefers creating a client-side discoverable credential, but will accept
 	 * a server-side credential.
 	 */
-	PREFERRED("preferred"),
+	public static final ResidentKeyRequirement PREFERRED = new ResidentKeyRequirement("preferred");
 
 	/**
 	 * The <a href="https://www.w3.org/TR/webauthn-3/#dom-residentkeyrequirement-required">required</a> value
 	 * indicates that the Relying Party requires a client-side discoverable credential.
 	 */
-	REQUIRED("required");
+	public static final ResidentKeyRequirement REQUIRED = new ResidentKeyRequirement("required");
 
 	private final String value;
 
-	ResidentKeyRequirement(String value) {
+	private ResidentKeyRequirement(String value) {
 		this.value = value;
 	}
 
@@ -58,4 +58,16 @@ public enum ResidentKeyRequirement {
 		return this.value;
 	}
 
+	public static ResidentKeyRequirement valueOf(String value) {
+		if (DISCOURAGED.getValue().equals(value)) {
+			return DISCOURAGED;
+		}
+		if (PREFERRED.getValue().equals(value)) {
+			return PREFERRED;
+		}
+		if (REQUIRED.getValue().equals(value)) {
+			return REQUIRED;
+		}
+		return new ResidentKeyRequirement(value);
+	}
 }
